942a9ebe275eea4054e1275cf65bc93db4eb8404
belupetri <belupetri01@gmail.com>
2025-05-28 18:01:53 -0300

    run

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev none,id=noaudio
+#!/bin/bash
+if [[ "$1"=="gdb" ]]; then
+      qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512  -d int -audiodev none,id=noaudio
+else
+      qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512  -audiodev none,id=noaudio
+
+fi
