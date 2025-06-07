6ed75397ac6a95617c1db430d2e4d85a126a24e8
belupetri <belupetri01@gmail.com>
2025-06-06 23:37:24 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$1"=="gdb" ]]; then
+if [[ "$1" == "gdb" ]]; then
       qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512  -d int -audiodev none,id=noaudio
 else
       qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512  -audiodev none,id=noaudio
